13cc502823b8ee6e33f93e9f802823dabc8f2097
joanvasquez21 <joanvrivas.21@gmail.com>
2025-02-14 19:45:33 +0100

    feat: the apikey is added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,9 @@ export const environment = {
     production: true,
     companyName: 'Gifs',
     companyName2: 'App',
-    companyName3: 'administrar los gifs'
+    companySlogan: 'Controla tus imágenes animadas',
+
+    giphyApiKey: ''
+
 
 };
