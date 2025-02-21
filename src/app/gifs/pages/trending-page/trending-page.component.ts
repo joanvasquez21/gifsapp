2cbbcdd24200200a4d7f63b673e828d53bd1b53a
joanvasquez21 <joanvrivas.21@gmail.com>
2025-02-21 20:12:16 +0100

    feat:implement viewchild in trendingpage component

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -2,16 +2,16 @@ import { Component, inject } from '@angular/core';
 import { GifListComponent } from "../../components/gif-list/gif-list.component";
 import { GifService } from '../../services/gifs.service';
 
-
-
 @Component({
   selector: 'app-trending-page',
-  imports: [GifListComponent],
+//  imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
   
   gifService = inject(GifService);
-  
+  onScroll(event: Event){
+    
+  }
 
  }
