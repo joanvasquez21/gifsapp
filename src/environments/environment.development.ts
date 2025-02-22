f9d3e19906d92038b11368943e34976d2bf16b1e
joanvasquez21 <joanvrivas.21@gmail.com>
2025-02-22 23:21:45 +0100

    feat: final changes are implemented

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    companyName: 'Gifs',
-    companyName2: 'App',
-    companySlogan: 'Controla tus imágenes animadas',
+    companyName: 'gifs',
+    companyName2: 'app',
+    companySlogan: 'La mejor colección de gifs en un solo lugar.',
 
     giphyApiKey: 'FgjsPjfhzn7fesR1bARkHlf22AikiH7H',
     giphyUrl: 'https://api.giphy.com/v1'
